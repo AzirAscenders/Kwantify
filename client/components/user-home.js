8becce8439bbf25a828553951646a8bbfe8df2ac
Yan Naing Lin <ynlin98@gmail.com>
2020-11-26 15:11:01 -0500

    Added database and pie chart

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -1,16 +1,34 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
+import {VictoryPie, VictoryLabel} from 'victory'
 
 /**
  * COMPONENT
  */
 export const UserHome = props => {
   const {email} = props
-
+  const sampleData = [
+    {x: 'Travel', y: 10},
+    {x: 'HealthCare', y: 20},
+    {x: 'Shopping', y: 30},
+    {x: 'Groceries', y: 10},
+    {x: 'Food & Drink', y: 20},
+    {x: 'Entertainment', y: 10}
+  ]
   return (
     <div>
       <h3>Welcome, {email}</h3>
+      <VictoryPie
+        padAngle={10}
+        innerRadius={20}
+        radius={50}
+        data={sampleData}
+        // width={400}
+        // height={400}
+        //style={{labels: {fontSize: 10}}}
+        style={{labels: {fontSize: 5, fill: 'red'}}}
+      />
     </div>
   )
 }
