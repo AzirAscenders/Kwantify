340ad465ae27c06e054bffe2abbb2bc26a8de0b2
Seong Jin Kim <seongjinkimsjk@gmail.com>
2020-11-29 20:26:02 -0500

    google vision basic setup complete

---
+++ server/api/vision.js
@@ -0,0 +1,22 @@
+const vision = require('@google-cloud/vision')
+const {GOOGLE_VISION_CREDENTIALS} = require('../../secrets')
+
+// clarify with Ben if this is a secret or not
+process.env.GOOGLE_APPLICATION_CREDENTIALS = GOOGLE_VISION_CREDENTIALS
+// Creates a client
+const client = new vision.ImageAnnotatorClient()
+
+/**
+ * TODO(developer): Uncomment the following line before running the sample.
+ */
+const fileName = '/Users/macbook/Fullstack/Capstone/Kwantify/sign_text.png'
+
+// Performs text detection on the local file
+async function visionReader() {
+  const [result] = await client.textDetection(fileName)
+  const detections = result.textAnnotations
+  console.log('Text:')
+  detections.forEach(text => console.log(text))
+}
+
+visionReader()
