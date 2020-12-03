9628f9ead2f9c246fb4c538e8c9b8e4cf3084d40
Yan Naing Lin <ynlin98@gmail.com>
2020-12-03 17:53:39 -0500

    we can now view all 90 day transactions that is sorted by date

--- client/components/Transactions.js
+++ client/components/Transactions.js
@@ -1,24 +1,73 @@
 import React from 'react'
 import {Table} from 'react-bootstrap'
+import {connect} from 'react-redux'
+import {fetchTransactions} from '../store/transactions'
 
-const Transactions = props => {
-  const transactions = props.transactions
-  console.log(transactions)
-  return (
-    <div>
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Date</th>
-            <th>Name</th>
-            <th>Category</th>
-            <th>Amount</th>
-          </tr>
-        </thead>
-      </Table>
-    </div>
-  )
+class Transactions extends React.Component {
+  constructor() {
+    super()
+    this.changeOption = this.changeOption.bind(this)
+  }
+
+  componentDidMount() {
+    this.props.fetchTransactions()
+  }
+
+  // changeOption(e) {
+  //   let transactions = this.props.transactions
+  //   if (e.target.value === 'currentMonth') {
+
+  //   } else if (e.target.value === 'lastMonth') {
+  //   } else if(e.target.value === 'twoMonthsAgo'){
+  //   }
+  // }
+
+  render() {
+    const transactions = this.props.transactions
+    return (
+      <div>
+        <h2>All Transactions</h2>
+        <select onChange={this.changeOption}>
+          <option value="all">All</option>
+          <option value="currentMonth">Current Month</option>
+          <option value="lastMonth">Last Month</option>
+          <option value="twoMonthsAgo">2 Month Ago</option>
+        </select>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Name</th>
+              <th>Category</th>
+              <th>Amount</th>
+            </tr>
+          </thead>
+          <tbody>
+            {transactions.map((transaction, idx) => (
+              <tr key={idx}>
+                <td>{transaction.date}</td>
+                <td>{transaction.name}</td>
+                <td>{transaction.category[0]}</td>
+                <td>${transaction.amount}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
+    )
+  }
 }
 
-export default Transactions
+const mapState = state => {
+  return {
+    transactions: state.transactions
+  }
+}
+
+const mapDispatch = dispatch => {
+  return {
+    fetchTransactions: () => dispatch(fetchTransactions())
+  }
+}
+
+export default connect(mapState, mapDispatch)(Transactions)
--- client/store/transactions.js
+++ client/store/transactions.js
@@ -35,22 +35,27 @@ export const fetchTransactions = () => async dispatch => {
  * REDUCER
  */
 export default function(state = defaultTransactions, action) {
+  let combinedTransactions = []
   switch (action.type) {
     case GET_TRANSACTIONS:
-      return action.transactions.map(bank => {
-        return bank.filter(
-          transaction =>
-            (transaction.category[0] === 'Transfer' &&
-              transaction.category[1] === 'Debit') ||
-            (transaction.category[0] === 'Payment' &&
-              transaction.category[1] !== 'Credit Card') ||
-            transaction.category[0] === 'Shops' ||
-            transaction.category[0] === 'Food and Drink' ||
-            transaction.category[0] === 'Travel' ||
-            transaction.category[0] === 'Recreation'
-        )
-      })
-
+      action.transactions
+        .map(bank => {
+          return bank.filter(
+            transaction =>
+              (transaction.category[0] === 'Transfer' &&
+                transaction.category[1] === 'Debit') ||
+              (transaction.category[0] === 'Payment' &&
+                transaction.category[1] !== 'Credit Card') ||
+              transaction.category[0] === 'Shops' ||
+              transaction.category[0] === 'Food and Drink' ||
+              transaction.category[0] === 'Travel' ||
+              transaction.category[0] === 'Recreation'
+          )
+        })
+        .map(bank => combinedTransactions.push(...bank))
+      return combinedTransactions.sort(
+        (a, b) => new Date(b.date) - new Date(a.date)
+      )
     default:
       return state
   }
