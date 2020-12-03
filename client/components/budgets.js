407e409acf3101294e100f44b557117084728f45
Tun Oo Swe <tunooswe@gmail.com>
2020-12-02 19:46:10 -0500

    total spending budget reliant upon other budgets

--- client/components/budgets.js
+++ client/components/budgets.js
@@ -10,13 +10,13 @@ class Budgets extends React.Component {
   constructor() {
     super()
     this.state = {
-      totalSpending: 0,
-      foodAndDrink: 0,
-      travel: 0,
-      entertainment: 0,
-      healthcare: 0,
-      shopping: 0,
-      groceries: 0
+      totalSpending: '',
+      foodAndDrink: '',
+      travel: '',
+      entertainment: '',
+      healthcare: '',
+      shopping: '',
+      groceries: ''
     }
   }
   componentDidMount() {
@@ -25,7 +25,9 @@ class Budgets extends React.Component {
 
   render() {
     const {budgets} = this.props
-    const budgetKeys = Object.keys(budgets).sort((a, b) => a - b)
+    const filteredBudget = Object.keys(budgets).filter(
+      budget => budget !== 'totalSpending'
+    )
 
     return (
       <div>
@@ -59,12 +61,19 @@ class Budgets extends React.Component {
               <td>{`$ ${(budgets.shopping / 100).toFixed(2)}`}</td>
             </tr>
             <tr>
-              <td>Total Spending</td>
-              <td>{`$ ${(budgets.totalSpending / 100).toFixed(2)}`}</td>
-            </tr>
-            <tr>
               <td>Travel</td>
               <td>{`$ ${(budgets.travel / 100).toFixed(2)}`}</td>
+            </tr>
+          </tbody>
+          <tbody>
+            <tr>
+              <td>Total Spending</td>
+              <td>{`$ ${(
+                filteredBudget.reduce(
+                  (accumulator, budget) => accumulator + budgets[budget],
+                  0
+                ) / 100
+              ).toFixed(2)}`}</td>
             </tr>
           </tbody>
         </Table>
