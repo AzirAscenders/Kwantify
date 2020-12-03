972e51b9e0147305b9ec01619be2954356cc7494
Tun Oo Swe <tunooswe@gmail.com>
2020-12-02 20:15:00 -0500

    working on budgets component

--- client/components/budgets.js
+++ client/components/budgets.js
@@ -18,9 +18,22 @@ class Budgets extends React.Component {
       shopping: '',
       groceries: ''
     }
+
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleChange = this.handleChange.bind(this)
   }
   componentDidMount() {
     this.props.fetchBudget()
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+  }
+
+  async handleChange(event) {
+    await this.setState({
+      [event.target.name]: event.target.value
+    })
   }
 
   render() {
@@ -31,52 +44,62 @@ class Budgets extends React.Component {
 
     return (
       <div>
-        <Table>
-          <thead>
-            <tr>
-              <th />
-              <th>Current Budget</th>
-              <th>Edit your Budget</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Entertainment</td>
-              <td>{`$ ${(budgets.entertainment / 100).toFixed(2)}`}</td>
-            </tr>
-            <tr>
-              <td>Food And Drink</td>
-              <td>{`$ ${(budgets.foodAndDrink / 100).toFixed(2)}`}</td>
-            </tr>
-            <tr>
-              <td>Groceries</td>
-              <td>{`$ ${(budgets.groceries / 100).toFixed(2)}`}</td>
-            </tr>
-            <tr>
-              <td>Healthcare</td>
-              <td>{`$ ${(budgets.healthcare / 100).toFixed(2)}`}</td>
-            </tr>
-            <tr>
-              <td>Shopping</td>
-              <td>{`$ ${(budgets.shopping / 100).toFixed(2)}`}</td>
-            </tr>
-            <tr>
-              <td>Travel</td>
-              <td>{`$ ${(budgets.travel / 100).toFixed(2)}`}</td>
-            </tr>
-          </tbody>
-          <tbody>
-            <tr>
-              <td>Total Spending</td>
-              <td>{`$ ${(
-                filteredBudget.reduce(
-                  (accumulator, budget) => accumulator + budgets[budget],
-                  0
-                ) / 100
-              ).toFixed(2)}`}</td>
-            </tr>
-          </tbody>
-        </Table>
+        <form>
+          <Table>
+            <thead>
+              <tr>
+                <th />
+                <th>Current Budget</th>
+                <th>Edit your Budget</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Entertainment</td>
+                <td>{`$ ${(budgets.entertainment / 100 || 0).toFixed(2)}`}</td>
+                <td>
+                  <input
+                    type="text"
+                    name="entertainment"
+                    value={this.state.entertainment}
+                    onChange={this.handleChange}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>Food And Drink</td>
+                <td>{`$ ${(budgets.foodAndDrink / 100 || 0).toFixed(2)}`}</td>
+              </tr>
+              <tr>
+                <td>Groceries</td>
+                <td>{`$ ${(budgets.groceries / 100 || 0).toFixed(2)}`}</td>
+              </tr>
+              <tr>
+                <td>Healthcare</td>
+                <td>{`$ ${(budgets.healthcare / 100 || 0).toFixed(2)}`}</td>
+              </tr>
+              <tr>
+                <td>Shopping</td>
+                <td>{`$ ${(budgets.shopping / 100 || 0).toFixed(2)}`}</td>
+              </tr>
+              <tr>
+                <td>Travel</td>
+                <td>{`$ ${(budgets.travel / 100 || 0).toFixed(2)}`}</td>
+              </tr>
+            </tbody>
+            <tbody>
+              <tr>
+                <td>Total Spending</td>
+                <td>{`$ ${(
+                  filteredBudget.reduce(
+                    (accumulator, budget) => accumulator + budgets[budget],
+                    0
+                  ) / 100
+                ).toFixed(2)}`}</td>
+              </tr>
+            </tbody>
+          </Table>
+        </form>
       </div>
     )
   }
