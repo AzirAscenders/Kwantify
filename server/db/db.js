f6727339c3eb348db08b2f6e9c9c67aa83d490ac
Seong Jin Kim <seongjinkimsjk@gmail.com>
2021-03-10 19:20:48 -0500

    heroku pg fix 2

--- server/db/db.js
+++ server/db/db.js
@@ -1,4 +1,3 @@
-const {stripLeadingSlash} = require('history/PathUtils')
 const Sequelize = require('sequelize')
 const pkg = require('../../package.json')
 
@@ -8,6 +7,11 @@ const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
   {
     logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true
+      }
+    },
     ssl: true
   }
 )
