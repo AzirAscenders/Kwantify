df13b7716645d63cc0d62f0b6e1d50d6a4250894
Seong Jin Kim <seongjinkimsjk@gmail.com>
2021-03-10 19:10:43 -0500

    heroku pg error

--- server/db/db.js
+++ server/db/db.js
@@ -1,3 +1,4 @@
+const {stripLeadingSlash} = require('history/PathUtils')
 const Sequelize = require('sequelize')
 const pkg = require('../../package.json')
 
@@ -6,7 +7,8 @@ const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
   {
-    logging: false
+    logging: false,
+    ssl: true
   }
 )
 module.exports = db
